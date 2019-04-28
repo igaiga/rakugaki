ce001e30e316484f8d33bec5832893ca60274259
Kuniaki IGARASHI <igaiga@gmail.com>
2019-04-28 14:55:12 +0900

    Add catche instance variable assign codes

---
+++ trace_point/catch_instance_variable_assign.rb
@@ -0,0 +1,60 @@
+# インスタンス変数へ代入された行を調べるコード
+
+# TracePointで止めてASTを確認し、インスタンス変数への代入かどうかを調べる
+def tracer(target_class_name, target_instance_variable_name)
+  TracePoint.trace(:line) do |tp|
+    # puts "[TP:#{tp.event}] #{tp.path}:#{tp.lineno} #{tp.method_id} #{tp.defined_class}"
+    line = File.open(tp.path, "r"){|f| f.readlines[tp.lineno - 1] } # もっと簡単にその行のコード取れないか？
+    next unless line
+
+    # AST取得
+    begin
+      node = RubyVM::AbstractSyntaxTree.parse(line).children.last
+    rescue Exception => e # 乱暴
+      next
+    end
+
+    # インスタンス変数への代入かを調べる
+    next unless node.type == :IASGN
+
+    # クラス名を調べる
+    target_class = Kernel.const_get(target_class_name)
+    next unless tp.self.is_a?(target_class)
+
+    # インスタンス変数名を調べる
+    instance_variable_name = node.children.first
+    next unless instance_variable_name == target_instance_variable_name.to_sym
+
+    puts "#{target_class_name} #{target_instance_variable_name} is assigned in #{tp.path}:#{tp.lineno} #{tp.method_id} #{tp.defined_class}"
+    puts "--- caller ---"
+    puts caller
+
+    # memo
+    # RubyVM::AbstractSyntaxTree.parse(line) #=> RubyVM::AbstractSyntaxTree:Node
+    #=> (SCOPE@1:0-1:14
+    # tbl: []
+    # args: nil
+    # body: (IASGN@1:4-1:14 :@hi (STR@1:10-1:14 "hi")))
+    # RubyVM::AbstractSyntaxTree.parse(line).children.last
+    #=> (IASGN@1:4-1:14 :@hi (STR@1:10-1:14 "hi"))
+    # RubyVM::AbstractSyntaxTree.parse(line).children.last #=> RubyVM::AbstractSyntaxTree::Node
+  end
+end
+
+module Greeting
+  def egao(text)
+    @greeting = text + "(｡･ω･｡)ノ"
+  end
+end
+
+class User
+  include Greeting
+  def hi
+    @hi = "hi"
+    egao "hi"
+  end
+end
+
+#tracer("User", "@hi")
+tracer("User", "@greeting")
+p User.new.hi
