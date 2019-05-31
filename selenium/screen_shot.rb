25313d50866c67de116a4c9138f993fd30a6876f
Kuniaki Igarashi <igaiga@gmail.com>
2019-05-31 10:17:41 +0900

    Add headless option

--- selenium/screen_shot.rb
+++ selenium/screen_shot.rb
@@ -1,7 +1,11 @@
 # gem i selenium-webdriver
 require "selenium-webdriver"
 
-driver = Selenium::WebDriver.for :chrome
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')
+# headless指定すると普段Chromeで指定しているフォント指定が効かなくなる
+
+driver = Selenium::WebDriver.for :chrome, options: options
 driver.get("https://igarashikuniaki.net/diary/")
 driver.save_screenshot("ss.png")
 driver.quit
